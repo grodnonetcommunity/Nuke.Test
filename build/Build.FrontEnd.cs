c5c890cb1bc4ceb40736d5311525cec94a9fbf7f
Alexander Puzynia <werwolf.by@gmail.com>
2018-08-07 23:39:48 +0300

    step4: add FrontEnd build tasks

---
+++ build/Build.FrontEnd.cs
@@ -0,0 +1,23 @@
+﻿using Nuke.Common;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Npm;
+using static Nuke.Common.Tools.Npm.NpmTasks;
+using static Nuke.Common.IO.PathConstruction;
+
+partial class Build
+{
+    AbsolutePath WebApp => SourceDirectory / "webapp";
+
+    Target FrontEndNpmInstall => _ => _
+        .Executes(() =>
+        {
+            NpmInstall(s => s.SetWorkingDirectory(WebApp));
+        });
+
+    Target FrontEndCompile => _ => _
+        .DependsOn(FrontEndNpmInstall)
+        .Executes(() =>
+        {
+            NpmRun(s => s.SetWorkingDirectory(WebApp).SetCommand("build"));
+        });
+}
--- build/Build.cs
+++ build/Build.cs
@@ -1,15 +1,13 @@
 using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.MSBuild;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 
-class Build : NukeBuild
+partial class Build : NukeBuild
 {
     public static int Main () => Execute<Build>(x => x.Compile);
 
